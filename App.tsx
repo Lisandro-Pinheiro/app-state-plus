041a456433fcbebc4c7e9b46e4ea718405405fdc
Lisandro-Pinheiro <lisandropinheiro10@gmail.com>
2023-05-31 21:27:05 -0300

    Meu Commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
 
-export default function App() {
+export default function App(){
+
+  const [fonteCarregada, setFonteCarregada] = useState(false);
+
+  async function loadFonts(){
+    await Font.loadAsync({
+      ubuntu_italic: require('./assets/fonts/Ubuntu-BoldItalic.ttf'),
+      ubuntu_regular: require('./assets/fonts/Ubuntu-Regular.ttf'),
+      ubuntu_medium: require('./assets/fonts/Ubuntu-Medium.ttf')
+    })
+  }
+
+  useEffect(()=>{
+    loadFonts()
+    .then(()=>{
+      setFonteCarregada(true);
+    })
+    .catch((error)=>{
+      console.log('Ocorreu um erro, error');
+    })
+  },[])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      {fonteCarregada ?
+      <Text style={{fontFamily:'ubuntu_regular', fontSize:30}}>Lisandro</Text>
+      :
+      <Text>Falso</Text>
+      }
+
       <StatusBar style="auto" />
+      
     </View>
   );
 }
@@ -17,4 +46,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
